1e18054b9aabb29432471ba187b4e297d6632521
Alexander Rafferty <21712241@ecm.uwa.edu.au>
2017-10-06 01:23:24 +0800

    Added test script

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+./percolate 1 f > results.csv
+./percolate 2 >> results.csv
+./percolate 3 >> results.csv
+./percolate 4 >> results.csv
+./percolate 5 >> results.csv
+./percolate 6 >> results.csv
+./percolate 7 >> results.csv
+./percolate 8 >> results.csv
+./percolate 9 >> results.csv
+./percolate 10 >> results.csv
+./percolate 11 >> results.csv
+./percolate 12 >> results.csv
+./percolate 13 >> results.csv
+./percolate 14 >> results.csv
+./percolate 15 >> results.csv
+./percolate 16 >> results.csv
+./percolate 17 >> results.csv
+./percolate 18 >> results.csv
+./percolate 19 >> results.csv
+./percolate 20 >> results.csv
+./percolate 21 >> results.csv
+./percolate 22 >> results.csv
+./percolate 23 >> results.csv
+./percolate 24 >> results.csv

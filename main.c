45427336ec44cdeee347e1a598b76983c6467ddb
Rafferty97 <alex@flashbang.io>
2017-10-05 21:36:46 +0800

    Added main

--- main.c
+++ main.c
@@ -6,7 +6,12 @@
 
 int main(int argc, char *argv[])
 {
-  percolation_results results;
-  site_percolation(4096, 0.6, atoi(argv[1]), &results);
-  print_results(results);
+  float time = 0;
+  for (int i = 0; i < 1000; i++) {
+    percolation_results results;
+    site_percolation(4096, 0.6, atoi(argv[1]), &results);
+    time += results.time_taken;
+    if ((i - 1) % 100 == 0) printf("%i iterations...\n", i - 1);
+  }
+  printf("4096 x 4096, 1000 iterations\n%i threads: %f seconds", atoi(argv[1]), time);
 }

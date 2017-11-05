4070d7eb1c7d2cfd5213bf6e071ebc118df68bfd
Rafferty97 <alex@flashbang.io>
2017-11-06 00:49:05 +0800

    Improved timing

--- percolate.c
+++ percolate.c
@@ -8,32 +8,28 @@
 #include <sys/time.h>
 #include <inttypes.h>
 
-struct timeval time_start, time_now;
+double t_alloc, t_seed, t_dfs, t_outline, t_stitch, t_percolate;
+
+double lap()
+{
+  static double start = 0;
+  double end = MPI_Wtime();
+  double diff = end - start;
+  start = end;
+  return diff;
+}
 
 outline percolate(char t, int sx, int sy, float p)
 {
-  // char filename[256];
-  // sprintf(filename, "grid%i,%i-%ix%i", grid_ox, grid_oy, sx, sy);
-  // FILE *f = fopen(filename, "w");
-  // fprintf(stdout, "Percolating a %i x %i %c grid at %i, %i.\n", sx, sy, t, grid_ox, grid_oy);
+  lap();
   grid g = alloc_grid(t, sx, sy);
+  t_alloc = lap();
   seed_grid(g, p);
-  gettimeofday(&time_now, NULL);
-  printf("%i seeded: %li\n", mpi_rank, 1000000 * (time_now.tv_sec - time_start.tv_sec) + (time_now.tv_usec - time_start.tv_usec));
-  // printf("%i, %i:\n", grid_ox, grid_oy);
-  // print_grid(stdout, g, false);
-  // printf("\n");
+  t_seed = lap();
   grid_do_dfs(&g);
-  gettimeofday(&time_now, NULL);
-  printf("%i DFS done: %li\n", mpi_rank, 1000000 * (time_now.tv_sec - time_start.tv_sec) + (time_now.tv_usec - time_start.tv_usec));
-  // print_grid(f, g, true);
-  // fprintf(f, "\n");
+  t_dfs = lap();
   outline o = outline_from_grid(g);
-  gettimeofday(&time_now, NULL);
-  printf("%i outlined: %li\n", mpi_rank, 1000000 * (time_now.tv_sec - time_start.tv_sec) + (time_now.tv_usec - time_start.tv_usec));
-  // print_outline(f, o);
-  // fprintf(f, "\n");
-  // fclose(f);
+  t_outline = lap();
   return o;
 }
 
@@ -54,14 +50,13 @@ outline percolate_mpi_rec(char t, int sx, int sy, float p, int nodes, int rank, 
       outline l = percolate_mpi_rec(t, sx_l, sy_l, p, nodes_l, rank, rank_start);
       if (rank != 0) return l;
       outline r = recv_outline(rank_start + nodes_l);
-      gettimeofday(&time_now, NULL);
-      printf("%i recv'd: %li\n", mpi_rank, 1000000 * (time_now.tv_sec - time_start.tv_sec) + (time_now.tv_usec - time_start.tv_usec));
-      // printf("Recieved grid %ix%i from %i to %i\n", r.sx, r.sy, rank_start + nodes_l, rank_start);
+      lap();
       if (horiz) {
         return merge_outlines_horiz(l, r);
       } else {
         return merge_outlines_vert(l, r);
       }
+      t_stitch += lap();
     } else {
       rank -= nodes_l;
       rank_start += nodes_l;
@@ -70,32 +65,31 @@ outline percolate_mpi_rec(char t, int sx, int sy, float p, int nodes, int rank, 
       if (horiz) grid_ox += dim_l; else grid_oy += dim_l;
       outline r = percolate_mpi_rec(t, sx_r, sy_r, p, nodes_r, rank, rank_start);
       if (rank == 0) {
-        // printf("Sending grid %ix%i from %i to %i\n", r.sx, r.sy, rank_start, rank_start - nodes_l);
         send_outline(r, rank_start - nodes_l);
-        gettimeofday(&time_now, NULL);
-        printf("%i sent: %li\n", mpi_rank, 1000000 * (time_now.tv_sec - time_start.tv_sec) + (time_now.tv_usec - time_start.tv_usec));
       }
       return r;
     }
   } else {
+    t_stitch = 0;
     return percolate(t, sx, sy, p);
   }
 }
 
 outline percolate_mpi(char t, int sx, int sy, float p, int nodes, int rank)
 {
-  gettimeofday(&time_start, NULL);
-  printf("%i start: %ju %ju\n", mpi_rank, (uintmax_t)time_start.tv_sec, (uintmax_t)time_start.tv_usec);
   grid_tx = sx;
   grid_ox = 0;
   grid_oy = 0;
   outline out = percolate_mpi_rec(t, sx, sy, p, nodes, rank, 0);
+  lap();
   if (rank == 0) {
-    gettimeofday(&time_now, NULL);
-    printf("%i recv'd: %li\n", mpi_rank, 1000000 * (time_now.tv_sec - time_start.tv_sec) + (time_now.tv_usec - time_start.tv_usec));
     percolate_outline(&out);
-    gettimeofday(&time_now, NULL);
-    printf("%i done: %li\n", mpi_rank, 1000000 * (time_now.tv_sec - time_start.tv_sec) + (time_now.tv_usec - time_start.tv_usec));
   }
+  t_percolate = lap();
+  char fn[256];
+  sprintf(fn, "rank%i", mpi_rank);
+  FILE *f = fopen(fn, "w");
+  fprintf(f, "alloc: %f\nseed: %f\ndfs: %f\noutline: %f\nstitch: %f\npercolate: %f\n", t_alloc, t_seed, t_dfs, t_outline, t_stitch, t_percolate);
+  fclose(f);
   return out;
 }

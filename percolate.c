2df0d2a9dac492d7623ef0d38817a6d329049f42
Rafferty97 <websites@alexanderrafferty.com>
2017-11-07 01:02:33 +0800

    Removed timing

--- percolate.c
+++ percolate.c
@@ -8,28 +8,12 @@
 #include <sys/time.h>
 #include <inttypes.h>
 
-double t_alloc, t_seed, t_dfs, t_outline, t_stitch, t_percolate;
-
-double lap()
-{
-  static double start = 0;
-  double end = MPI_Wtime();
-  double diff = end - start;
-  start = end;
-  return diff;
-}
-
 outline percolate(char t, int sx, int sy, float p)
 {
-  lap();
   grid g = alloc_grid(t, sx, sy);
-  t_alloc = lap();
   seed_grid(g, p);
-  t_seed = lap();
   grid_do_dfs(&g);
-  t_dfs = lap();
   outline o = outline_from_grid(g);
-  t_outline = lap();
   return o;
 }
 
@@ -50,14 +34,12 @@ outline percolate_mpi_rec(char t, int sx, int sy, float p, int nodes, int rank, 
       outline l = percolate_mpi_rec(t, sx_l, sy_l, p, nodes_l, rank, rank_start);
       if (rank != 0) return l;
       outline r = recv_outline(rank_start + nodes_l);
-      lap();
       outline comb;
       if (horiz) {
         comb = merge_outlines_horiz(l, r);
       } else {
         comb = merge_outlines_vert(l, r);
       }
-      t_stitch += lap();
       return comb;
     } else {
       rank -= nodes_l;
@@ -72,7 +54,6 @@ outline percolate_mpi_rec(char t, int sx, int sy, float p, int nodes, int rank, 
       return r;
     }
   } else {
-    t_stitch = 0;
     return percolate(t, sx, sy, p);
   }
 }
@@ -83,15 +64,8 @@ outline percolate_mpi(char t, int sx, int sy, float p, int nodes, int rank)
   grid_ox = 0;
   grid_oy = 0;
   outline out = percolate_mpi_rec(t, sx, sy, p, nodes, rank, 0);
-  lap();
   if (rank == 0) {
     percolate_outline(&out);
   }
-  t_percolate = lap();
-  char fn[256];
-  sprintf(fn, "rank%i", mpi_rank);
-  FILE *f = fopen(fn, "w");
-  fprintf(f, "alloc: %f\nseed: %f\ndfs: %f\noutline: %f\nstitch: %f\npercolate: %f\n", t_alloc, t_seed, t_dfs, t_outline, t_stitch, t_percolate);
-  fclose(f);
   return out;
 }
